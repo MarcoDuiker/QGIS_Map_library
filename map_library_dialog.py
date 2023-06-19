5978c23b4e722a20a0330975d9852711a36f1120
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2023-06-19 11:23:25 +0200

    Remove search string when closing dialog

--- map_library_dialog.py
+++ map_library_dialog.py
@@ -32,7 +32,7 @@ FORM_CLASS, _ = uic.loadUiType(os.path.join(
 
 
 class MapLibraryDialog(QtWidgets.QDialog, FORM_CLASS):
-    def __init__(self, parent=None):
+    def __init__(self, mapLibrary, parent=None):
         """Constructor."""
         super(MapLibraryDialog, self).__init__(parent)
         # Set up the user interface from Designer.
@@ -40,4 +40,9 @@ class MapLibraryDialog(QtWidgets.QDialog, FORM_CLASS):
         # self.<objectname>, and you can use autoconnect slots - see
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
+        self.mapLibrary = mapLibrary
         self.setupUi(self)
+
+    def closeEvent(self, event):
+        self.mapLibrary.close_dialog()
+        super(MapLibraryDialog, self).closeEvent(event)
